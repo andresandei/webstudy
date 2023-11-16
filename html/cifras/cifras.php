c4b13418000030e03a292a8e6531c27fd953c5e4
Sandei <andresandei@yahoo.com.br>
2023-11-16 16:08:12 -0300

    Update cifras.php

--- html/cifras/cifras.php
+++ html/cifras/cifras.php
@@ -5,7 +5,7 @@
     $password = "@Cifras123";
     $dbname = "u242803527_cifras";
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
+    $conn = new mysqli($servername, $username, $dbname);
 
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
