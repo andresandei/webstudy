c3f055669e4dcacda862115cb5fbb20b62ed9f8b
Sandei <andresandei@yahoo.com.br>
2023-11-16 14:15:25 -0300

    adding php retriving music

---
+++ html/cifras/cifras.php
@@ -0,0 +1,27 @@
+<?php
+    $servername = "151.106.97.204";
+    $username = "u242803527_sandei";
+    $password = "@Cifras123";
+    $dbname = "u242803527_cifras";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT id, nome, path FROM users";
+    $result = $conn->query($sql);
+
+    $data = [];
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $data[] = $row;
+        }
+    }
+
+    $conn->close();
+
+    echo json_encode($data);
+?>
---
+++ js/js/cifras.js
@@ -0,0 +1,25 @@
+// Initialize an empty array to store user data
+var userDataArray = [];
+
+// Use fetch to call the PHP script and get data
+fetch('cifras.php')
+    .then(response => response.json())
+    .then(data => {
+        // Store data in the JavaScript array
+        userDataArray = data;
+
+        // Display data in the HTML
+        displayData(userDataArray);
+    })
+    .catch(error => console.error('Error:', error));
+
+// Display data in the HTML
+function displayData(data) {
+    var userList = document.getElementById('musicList');
+
+    data.forEach(function (user) {
+        var userDiv = document.createElement('div');
+        userDiv.innerHTML = 'ID: ' + user.id + '<br>Name: ' + user.name + '<br>Email: ' + user.email + '<hr>';
+        userList.appendChild(userDiv);
+    });
+}
