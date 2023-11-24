72181812f7f561cc4f595cf5064844d1d003634d
Sandei <andresandei@yahoo.com.br>
2023-11-24 16:13:38 -0300

    up

--- html/cifras/register.php
+++ html/cifras/register.php
@@ -26,10 +26,56 @@ $row=mysqli_fetch_row($lastresult);
 // Handle image upload
 $summed = $row[0]+1;
 $filenameR = "img". $summed . "-" . "0";
-$targetDir = "../../img/cifras/"; // Create an "uploads" folder in your project
+$target_dir = "../../img/cifras/"; // Create an "uploads" folder in your project
 $targetFile = $targetDir . $filenameR . ".jpeg"; // Unique file name
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-move_uploaded_file($_FILES["itemImage"][$filename], $targetFile);
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+  if($check !== false) {
+    echo "File is an image - " . $check["mime"] . ".";
+    $uploadOk = 1;
+  } else {
+    echo "File is not an image.";
+    $uploadOk = 0;
+  }
+}
+
+// Check if file already exists
+if (file_exists($target_file)) {
+  echo "Sorry, file already exists.";
+  $uploadOk = 0;
+}
+
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+  echo "Sorry, your file is too large.";
+  $uploadOk = 0;
+}
+
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+  $uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+  }
+}
+
+//move_uploaded_file($_FILES["itemImage"]["tmp_name"], $targetFile);
 
 // Insert data into the database
 $sql = "INSERT INTO musicas (nome, path, tom) VALUES ('$itemName', '$filenameR', '$itemTono')";
