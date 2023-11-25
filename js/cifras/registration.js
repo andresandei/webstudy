0dac47bcbf553b06b5639e4876e11e2bd7b4ce29
Sandei <andresandei@yahoo.com.br>
2023-11-25 18:49:41 -0300

    upds

--- js/cifras/registration.js
+++ js/cifras/registration.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("registrationForm").addEventListener("submit", function (event) {
-        event.preventDefault(); // Evita o envio padrão do formulário
+        event.preventDefault(); // Prevent the default form submission
 
         // Get form data
         var itemName = document.getElementById("itemName").value;
-        var itemImage = document.getElementById("itemImage").files[0]; // Arquivo do input
+        var itemImage = document.getElementById("itemImage").files[0]; // File input
         var itemTono = document.getElementById("itemTono").value;
 
         // Create FormData object to send files
@@ -18,56 +18,52 @@ document.addEventListener("DOMContentLoaded", function () {
             method: "POST",
             body: formData
         })
-        .then(response => response.text()) // Converte a resposta para texto
+        .then(response => response.text()) // Convert response to text
         .then(data => {
-            console.log(data);  // Log da resposta bruta
-
             try {
                 var jsonData = JSON.parse(data);
-                
-                // Exibe a notificação
-                displayNotification(jsonData.status, jsonData.message);
+        
+                // Display the notification immediately after clicking Register
+                displayNotification(jsonData.status, "Cadastrando...");
 
+                // If successful, update the notification content and style
+                if (jsonData.status === 'success') {
+                    displayNotification(jsonData.status, jsonData.message);
+                }
             } catch (error) {
-                console.error('Erro ao analisar JSON:', error);
+                console.error('Error parsing JSON:', error);
             }
         })
-        .catch(error => console.error('Erro:', error));
+        .catch(error => console.error('Error:', error));
+    });
+
+    // OK button click event to hide the notification
+    document.getElementById("okButton").addEventListener("click", function () {
+        document.getElementById('notificationContainer').style.display = 'none';
     });
 });
 
 function displayNotification(status, message) {
     var notificationContainer = document.getElementById('notificationContainer');
     var notificationContent = document.getElementById('notificationContent');
-    var okButton = document.getElementById('okButton');
 
-    // Defina o conteúdo e o estilo com base no status...
+    // Set the content and style based on the status
+    notificationContent.innerHTML = message;
+    notificationContainer.className = ''; // Clear existing classes
 
-    // Exibe a notificação
-    notificationContainer.style.display = 'flex';
-    document.body.style.overflow = 'hidden';
-
-    // Se o status for "success", altera o texto para "Completo" e exibe o botão OK
     if (status === 'success') {
-        notificationContent.innerHTML = 'Completo';
-        okButton.style.display = 'block';
-    } else {
-        // Caso contrário, mantém o texto original
-        notificationContent.innerHTML = message;
-        okButton.style.display = 'none';
+        notificationContainer.classList.add('success');
+    } else if (status === 'error') {
+        notificationContainer.classList.add('error');
     }
 
-    // Atacha o evento de clique ao botão OK
-    okButton.addEventListener('click', function () {
-        closeNotification(notificationContainer);
-    });
-}
+    // Display the notification
+    notificationContainer.style.display = 'block';
 
-function closeNotification(container) {
-    container.style.display = 'none';
-    document.body.style.overflow = 'auto';
+    // If successful, hide the notification after a few seconds
+    if (status === 'success') {
+        setTimeout(function () {
+            notificationContainer.style.display = 'none';
+        }, 3000); // 3 seconds
+    }
 }
-
-//TODO: Make a feedback of the registering process
-//TODO: Make a way to add more than 1 image
-//TODO: Make to delete a song from the list?
