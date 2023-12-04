1185e51a0371f1f624e9325d6df8b4681a7a6eea
Sandei <andresandei@yahoo.com.br>
2023-12-04 18:17:58 -0300

    fix

--- html/cifras/register.php
+++ html/cifras/register.php
@@ -14,22 +14,20 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-function isImageTypeAllowed($file) {
-    $allowedFormats = ["jpg", "jpeg", "png", "gif", "heif"];
-    $imageFileType = strtolower(pathinfo($file['name'], PATHINFO_EXTENSION));
-    return in_array($imageFileType, $allowedFormats);
-}
-
-function isImageSizeAllowed($file) {
-    // Adjust the maximum allowed size (in bytes) as needed
-    $maxSize = 500000; // 500 KB
-    return $file && $file['size'] <= $maxSize;
-}
-
 // Handle form data
 $itemName = $_POST['itemName'];
 $itemImages = $_FILES['itemImages'];
 $itemTono = $_POST['itemTono'];
+
+// Handle image upload
+$lastsql = "SELECT id from musicas ORDER BY id DESC LIMIT 1;";
+$lastresult = $conn->query($lastsql);
+$row = mysqli_fetch_row($lastresult);
+
+$summed = $row[0] + 1;
+$target_dir = "../../img/cifras/";
+$uploadOk = 1;
+$imagePaths = array(); // Array to store image paths
 
 // Loop through each image
 for ($i = 0; $i < count($itemImages['name']); $i++) {
@@ -53,28 +51,6 @@ for ($i = 0; $i < count($itemImages['name']); $i++) {
         exit;
     }
 
-    // Continue with the rest of the processing for this image...
-}
-
-// Handle image upload
-$lastsql = "SELECT id from musicas ORDER BY id DESC LIMIT 1;";
-$lastresult = $conn->query($lastsql);
-$row = mysqli_fetch_row($lastresult);
-
-$summed = $row[0] + 1;
-$target_dir = "../../img/cifras/";
-$uploadOk = 1;
-
-// Loop through each image
-for ($i = 0; $i < count($itemImages['name']); $i++) {
-    $currentImage = array(
-        'name' => $itemImages['name'][$i],
-        'type' => $itemImages['type'][$i],
-        'tmp_name' => $itemImages['tmp_name'][$i],
-        'error' => $itemImages['error'][$i],
-        'size' => $itemImages['size'][$i]
-    );
-
     $target_file = $target_dir . basename($currentImage['name']);
     $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
     $filenameR = "img" . $summed . "-" . $i . "." . $imageFileType;
@@ -86,42 +62,26 @@ for ($i = 0; $i < count($itemImages['name']); $i++) {
         exit;
     }
 
-    // Check if image file is a valid image
-    $check = getimagesize($currentImage['tmp_name']);
-    if ($check === false) {
-        echo json_encode(array("status" => "error", "message" => "File is not an image."));
-        exit;
-    }
-
-    // Check file size
-    if ($currentImage['size'] > 500000) {
-        echo json_encode(array("status" => "error", "message" => "Sorry, your file is too large."));
-        exit;
-    }
-
-    // Allow certain file formats
-    $allowedFormats = array("jpg", "jpeg", "png", "gif", "heif");
-    if (!in_array($imageFileType, $allowedFormats)) {
-        echo json_encode(array("status" => "error", "message" => "Sorry, only JPG, JPEG, PNG, GIF & HEIF files are allowed."));
-        exit;
-    }
-
     if (move_uploaded_file($currentImage['tmp_name'], $targetFile)) {
         // File uploaded successfully
-        $response = array("status" => "success", "message" => "The file " . basename($currentImage['name']) . " has been uploaded.");
+        $imagePaths[] = $filenameR; // Store the path in the array
     } else {
         // Error uploading file
-        $response = array("status" => "error", "message" => "Sorry, there was an error uploading your file.");
+        echo json_encode(array("status" => "error", "message" => "Sorry, there was an error uploading your file."));
         exit;
     }
+}
 
-    // Insert data into the database
-    $sql = "INSERT INTO musicas (nome, path, tom) VALUES ('$itemName', '$filenameR', '$itemTono')";
+// Concatenate the image paths with "*"
+$imagePathsString = implode("*", $imagePaths);
 
-    if ($conn->query($sql) !== TRUE) {
-        $response = array("status" => "error", "message" => "Error: " . $sql . "<br>" . $conn->error);
-        exit;
-    }
+// Insert data into the database
+$sql = "INSERT INTO musicas (nome, path, tom) VALUES ('$itemName', '$imagePathsString', '$itemTono')";
+
+if ($conn->query($sql) === TRUE) {
+    $response = array("status" => "success", "message" => "Item registered successfully");
+} else {
+    $response = array("status" => "error", "message" => "Error: " . $sql . "<br>" . $conn->error);
 }
 
 // Close the connection
